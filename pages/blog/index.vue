93ebf62cae1e21c1743e1f03bea2cffdc7629756
Jakob Vendegna <jakob.vendegna@dotedu.tech>
2019-11-15 11:40:56 -0700

    add blog article traefik-consul-config

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -11,8 +11,12 @@
         <li class="links">
           <a href="blog/pipelines-first" class="links">A Pipelines-First Approach to DevOps</a>
         </li>
+        <li class="links">
+          <a href="blog/ha-traefik-consul-config" class="links">HA Traefik as a Kubernetes Ingress Controller With A Consul Backend</a>
+        </li>
       </ul>
       <nuxt-child name="blog/pipelines-first" />
+      <nuxt-child name="blog/ha-traefik-consul-config" />
     </div>
   </div>
 </template>
